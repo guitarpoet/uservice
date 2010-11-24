8a9651d6f6a11b05ae9d3abdeafef1db0f3696a4
Jack <guitarpoet@gmail.com>
2010-11-24 15:34:54 +0800

    Added the data schema into git

--- data.sql
+++ data.sql
@@ -1,31 +1,117 @@
-create table if not exists users (
-	id integer primary key auto_increment,
-	email varchar(64) not null unique,
-	username varchar(64) not null unique,
-	password varchar(16) not null,
-	register_time datetime,
-	register_ip varchar(16),
-	last_login_ip varchar(16),
-	last_login_time datetime,
-	login_count integer default 0,
-	salt char(6),
-	status tinyint(1)
-);
+-- MySQL dump 10.11
+--
+-- Host: localhost    Database: uservice
+-- ------------------------------------------------------
+-- Server version	5.0.51b
 
-create table if not exists groups (
-	id integer primary key auto_increment,
-	groupname varchar(64) not null unique,
-	description varchar(1024),
-	creation_time datetime,
-	last_modification_time datetime,
-	creator integer not null,
-	foreign key (creator) references users (id)
-);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-create table if not exists group_members (
-	uid integer,
-	gid integer,
-	primary key (uid, gid),
-	foreign key (uid) references users (id),
-	foreign key (gid) references groups (id)
-);
+--
+-- Table structure for table `group_members`
+--
+
+DROP TABLE IF EXISTS `group_members`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `group_members` (
+  `uid` int(11) NOT NULL default '0',
+  `gid` int(11) NOT NULL default '0',
+  PRIMARY KEY  (`uid`,`gid`),
+  KEY `gid` (`gid`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `group_members`
+--
+
+LOCK TABLES `group_members` WRITE;
+/*!40000 ALTER TABLE `group_members` DISABLE KEYS */;
+INSERT INTO `group_members` VALUES (2,1),(2,2);
+/*!40000 ALTER TABLE `group_members` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL auto_increment,
+  `groupname` varchar(64) NOT NULL,
+  `description` varchar(1024) default NULL,
+  `creation_time` datetime default NULL,
+  `last_modification_time` datetime default NULL,
+  `creator` int(11) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `groupname` (`groupname`),
+  KEY `creator` (`creator`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES (1,'administrators','This is the group for administrators.','2010-11-05 14:35:47','2010-11-24 14:44:54',2);
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL auto_increment,
+  `email` varchar(64) NOT NULL,
+  `username` varchar(64) NOT NULL,
+  `password` varchar(16) NOT NULL,
+  `register_time` datetime default NULL,
+  `register_ip` varchar(16) default NULL,
+  `last_login_ip` varchar(16) default NULL,
+  `last_login_time` datetime default NULL,
+  `login_count` int(11) default '0',
+  `salt` char(6) default NULL,
+  `status` tinyint(1) default NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'guitarpoet@gmail.com','Jack','hello',NULL,NULL,NULL,NULL,0,NULL,1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2010-11-24  7:34:19
